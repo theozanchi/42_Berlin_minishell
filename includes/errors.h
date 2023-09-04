bbd4e97d47dfaaa13ff48eb89ceffbd7ec936301
tzanchi <tzanchi@student.42berlin.de>
2023-09-04 15:32:46 +0200

    add first error msg

--- includes/errors.h
+++ includes/errors.h
@@ -6,11 +6,13 @@
 /*   By: tzanchi <tzanchi@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 12:47:36 by tzanchi           #+#    #+#             */
-/*   Updated: 2023/09/04 12:48:48 by tzanchi          ###   ########.fr       */
+/*   Updated: 2023/09/04 15:32:32 by tzanchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERRORS_H
 # define ERRORS_H
+
+# define ERR_ARG_NR "Wrong number of arguments\n"
 
 #endif
