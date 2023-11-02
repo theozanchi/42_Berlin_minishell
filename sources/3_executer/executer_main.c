fee81feef8de414c89aafad5ff3558a7061587fc
Jakob Schott <jschott@student.42berlin.de>
2023-11-02 12:29:38 +0100

    reworked executer logic to fork & pipe

--- sources/3_executer/executer_main.c
+++ sources/3_executer/executer_main.c
@@ -1,16 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   executer.c                                         :+:      :+:    :+:   */
+/*   executer_main.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jschott <jschott@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 16:22:45 by jschott           #+#    #+#             */
-/*   Updated: 2023/11/01 18:36:43 by jschott          ###   ########.fr       */
+/*   Updated: 2023/11/02 12:16:17 by jschott          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executer.h"
+#include "minishell.h"
 
 void	pipe2fd(t_output **out, t_commands *cmd, int *fd_pipe, char **env)
 {
@@ -67,65 +68,50 @@ void	fd2pipe(int *fd_pipe, t_commands *cmd, t_input **in, char **env)
 	close (fd_in);
 }
 
-int	executer(t_output **out_redirect, t_commands *cmds, t_input **in_redirect, char **env)
+int	cmd_count(t_commands *cmds)
 {
-	int	i;
-	int	fd_pipe_0[2];
-	int	fd_pipe_1[2];
-	int	pid;
+	int			i;
+	t_commands	*ptr;
 
 	i = 0;
-/* 
- * as many processes as commands 
- */
-	if (cmds->next)
+	ptr = cmds;
+	while (ptr)
 	{
-		pipe(fd_pipe_0);
-			// MISSING ERROR EXIT WHEN PIPING FAILS
+		ptr = ptr->next;
+		i++;
+	}
+	return (i);
+}
+
+int	executer(t_output **out_redirect, t_commands *cmds, t_input **in_redirect, char **env)
+{
+	int		i;
+	int		cmds_num;
+	int		**fd_pipes;
+	pid_t	pid;
+
+	cmds_num = cmd_count(cmds);
+	i = 0;
+	fd_pipes = ft_calloc(cmds_num, 2 * sizeof(int));
+	while (cmds->next)
+	{
+		pipe(fd_pipes[i]);
+			// ERROR MGMT TBD
 		pid = fork();
 		if (pid == -1)
 			return (0); // ERROR MGMT TBD
-		if (pid == 0)
+		if (pid == 0 && i > 0)
+			pipe2pipe(fd_pipes[i - 1], cmds, fd_pipes[i], env);
+		else if (pid == 0)
 			fd2pipe(pid, cmds, in_redirect, env);
 		if (pid != 0)
-		{
-			cmds = cmds->next;
-			while (cmds->next)
-			{
-				pipe(fd_pipe_1);
-				pid = fork();
-				if (pid == -1)
-					return (0); // ERROR MGMT TBD
-				if (pid == 0)
-					pipe2pipe(fd_pipe_1, cmds, fd_pipe_0, env);
-				if (pid != 0)
-					wait (NULL)
-					
-
-			}
-		}
+			wait (NULL);
+		++i;
+		cmds = cmds->next;
 	}
-/* 
- * create and/or open all given output files 
- */
 	i = -1;
 	while (out_redirect[++i])
-	{
-		fd_out = open(out_redirect[i], O_WRONLY | O_CREAT | O_TRUNC, 0666);
-		read_pipe (cmds, fd_out, fd_pipe, env);
-	}
-}
-
-
-int	execute(int *fd_out, int *fd_in, t_commands cmd, char **env)
-{
-	if (fd_in <= 0)
-		exit ; // ERROR MGMT TBD
-	dup2(fd_in[0], 0);
-	close(fd_in[1]);
-	dup2(fd_out[1], 1);
-	close(fd_out[0]);
-	exec(cmd, env);
-	close(fd_in[0]);
+		pipe2fd (out_redirect[i], cmds, fd_pipes[i], env);
+	free (fd_pipes);
 	return (1);
 }
