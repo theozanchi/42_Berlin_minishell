d09abef699f904d6b3e976e34a74253c2b7bda81
tzanchi <tzanchi@student.42berlin.de>
2023-11-15 15:47:35 +0100

    bug fix

--- sources/2_parser/parser/populate_node.c
+++ sources/2_parser/parser/populate_node.c
@@ -6,7 +6,7 @@
 /*   By: tzanchi <tzanchi@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 11:43:17 by tzanchi           #+#    #+#             */
-/*   Updated: 2023/11/15 15:45:31 by tzanchi          ###   ########.fr       */
+/*   Updated: 2023/11/15 15:47:23 by tzanchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ int	populate_node_command(t_commands *node, t_token *token)
 	node->command = ft_strdup(token->value);
 	if (!node->command)
 		return (EXIT_FAILURE);
+	node->cmd_quote = token->quote;
 	return (EXIT_SUCCESS);
 }
 
@@ -40,7 +41,7 @@ int	populate_node_flag(t_commands *node, t_token *token)
 {
 	if (!node)
 		return (EXIT_FAILURE);
-	if (add_new_list_node(&node->flags, token->value))
+	if (add_new_list_node(&node->flags, token))
 		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
@@ -56,7 +57,7 @@ int	populate_node_argument(t_commands *node, t_token *token)
 {
 	if (!node)
 		return (EXIT_FAILURE);
-	if (add_new_list_node(&node->arguments, token->value))
+	if (add_new_list_node(&node->arguments, token))
 		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
