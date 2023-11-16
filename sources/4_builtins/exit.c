e598f2a8e451d989f0ce27946077a9dd1f42edc9
tzanchi <tzanchi@student.42berlin.de>
2023-11-16 11:10:16 +0100

    added comments

--- sources/4_builtins/exit.c
+++ sources/4_builtins/exit.c
@@ -6,12 +6,20 @@
 /*   By: tzanchi <tzanchi@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 10:56:06 by tzanchi           #+#    #+#             */
-/*   Updated: 2023/11/16 11:05:58 by tzanchi          ###   ########.fr       */
+/*   Updated: 2023/11/16 11:10:03 by tzanchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/**
+ * @brief Check that the argument for the exit function are valid: one max,
+ * numeric
+ * 
+ * @param c The current command node
+ * @param data The main data structure
+ * @return EXIT_SUCCESS or EXIT_FAILURE
+ */
 static int	check_arg(t_commands *c, t_data *data)
 {
 	if (c->flags)
@@ -35,6 +43,15 @@ static int	check_arg(t_commands *c, t_data *data)
 	return (EXIT_SUCCESS);
 }
 
+/**
+ * @brief The exit builtin  cause minishell to exit from its current execution
+ * environment with the exit status specified by the unsigned decimal integer n
+ * passed as an argument or EXIT_SUCCESS if not specified 
+ * 
+ * @param c The current command node
+ * @param data The main data structure
+ * @return EXIT_SUCCESS or EXIT_FAILURE
+ */
 int	builtin_exit(t_commands *c, t_data *data)
 {
 	if (check_arg(c, data))
